7e3d1299a7fbeb94e819350a5cc91887adb5a404
qweriikk <dashakots13@gmail.com>
2023-06-12 18:30:52 +0300

    hm4

--- movies_tasks.sql
+++ movies_tasks.sql
@@ -1,20 +1,20 @@
--- 1. Вывести сколько фильмов сняла кинокомпания Universal Pictures.
+-- 1. Р’С‹РІРµСЃС‚Рё СЃРєРѕР»СЊРєРѕ С„РёР»СЊРјРѕРІ СЃРЅСЏР»Р° РєРёРЅРѕРєРѕРјРїР°РЅРёСЏ Universal Pictures.
 select count(movies.title) from movies join companies on companies.id = movies.companies_id where companies.title = "Universal Pictures";
--- 2. Вывести сколько всего фильмов было снято режиссером Фрэнсисом Фордом Копполой.
-select count(movies.title) from movies join directors on directors.id = movies.directors_id where directors.full_name = "Фрэнсис Форд Коппола";
--- 3. Вывести количество снятых фильмов за последние 20 лет.
+-- 2. Р’С‹РІРµСЃС‚Рё СЃРєРѕР»СЊРєРѕ РІСЃРµРіРѕ С„РёР»СЊРјРѕРІ Р±С‹Р»Рѕ СЃРЅСЏС‚Рѕ СЂРµР¶РёСЃСЃРµСЂРѕРј Р¤СЂСЌРЅСЃРёСЃРѕРј Р¤РѕСЂРґРѕРј РљРѕРїРїРѕР»РѕР№.
+select count(movies.title) from movies join directors on directors.id = movies.directors_id where directors.full_name = "Р¤СЂСЌРЅСЃРёСЃ Р¤РѕСЂРґ РљРѕРїРїРѕР»Р°";
+-- 3. Р’С‹РІРµСЃС‚Рё РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРЅСЏС‚С‹С… С„РёР»СЊРјРѕРІ Р·Р° РїРѕСЃР»РµРґРЅРёРµ 20 Р»РµС‚.
 select count(*) from movies where year between (year(curdate()) - 20) and year(curdate());
--- 4. Вывести все жанры фильмов в которых снимал Стивен Спилберг в течении всей своей карьеры. 
-select distinct genres.title from genres join movies on genres.id = movies.genres_id join directors on directors.id = movies.directors_id where directors.full_name = "Стивен Спилберг";
--- 5. Вывести названия, жанры и режиссеров 5 самых дорогих фильмов.
+-- 4. Р’С‹РІРµСЃС‚Рё РІСЃРµ Р¶Р°РЅСЂС‹ С„РёР»СЊРјРѕРІ РІ РєРѕС‚РѕСЂС‹С… СЃРЅРёРјР°Р» РЎС‚РёРІРµРЅ РЎРїРёР»Р±РµСЂРі РІ С‚РµС‡РµРЅРёРё РІСЃРµР№ СЃРІРѕРµР№ РєР°СЂСЊРµСЂС‹. 
+select distinct genres.title from genres join movies on genres.id = movies.genres_id join directors on directors.id = movies.directors_id where directors.full_name = "РЎС‚РёРІРµРЅ РЎРїРёР»Р±РµСЂРі";
+-- 5. Р’С‹РІРµСЃС‚Рё РЅР°Р·РІР°РЅРёСЏ, Р¶Р°РЅСЂС‹ Рё СЂРµР¶РёСЃСЃРµСЂРѕРІ 5 СЃР°РјС‹С… РґРѕСЂРѕРіРёС… С„РёР»СЊРјРѕРІ.
 select movies.title, genres.title, directors.full_name from movies join genres on genres.id = movies.genres_id join directors on directors.id = directors_id order by budget desc limit 5;
--- 6. Вывести имя режиссера с самым большим количеством фильмов.
+-- 6. Р’С‹РІРµСЃС‚Рё РёРјСЏ СЂРµР¶РёСЃСЃРµСЂР° СЃ СЃР°РјС‹Рј Р±РѕР»СЊС€РёРј РєРѕР»РёС‡РµСЃС‚РІРѕРј С„РёР»СЊРјРѕРІ.
 select directors.full_name from directors join movies on directors.id = directors_id group by directors.full_name order by count(movies.title) desc limit 1;
--- 7.Вывести названия и жанры фильмов, снятые самой большой кинокомпанией (по сумме всех бюджетов фильмов)
+-- 7.Р’С‹РІРµСЃС‚Рё РЅР°Р·РІР°РЅРёСЏ Рё Р¶Р°РЅСЂС‹ С„РёР»СЊРјРѕРІ, СЃРЅСЏС‚С‹Рµ СЃР°РјРѕР№ Р±РѕР»СЊС€РѕР№ РєРёРЅРѕРєРѕРјРїР°РЅРёРµР№ (РїРѕ СЃСѓРјРјРµ РІСЃРµС… Р±СЋРґР¶РµС‚РѕРІ С„РёР»СЊРјРѕРІ)
 select movies.title, genres.title from movies join genres on genres.id = movies.genres_id where companies_id = (select companies.id from companies join movies on movies.companies_id = companies.id group by companies.id order by sum(movies.budget) desc limit 1);
--- 8. Вывести средний бюджет фильмов, снятых кинокомпанией Warner Bros.
+-- 8. Р’С‹РІРµСЃС‚Рё СЃСЂРµРґРЅРёР№ Р±СЋРґР¶РµС‚ С„РёР»СЊРјРѕРІ, СЃРЅСЏС‚С‹С… РєРёРЅРѕРєРѕРјРїР°РЅРёРµР№ Warner Bros.
 select avg(movies.budget) from movies join companies on companies.id = companies_id where companies.title = "Warner Bros.";
--- 9. Вывести количество фильмов каждого жанра и средний бюджет по жанру. 
+-- 9. Р’С‹РІРµСЃС‚Рё РєРѕР»РёС‡РµСЃС‚РІРѕ С„РёР»СЊРјРѕРІ РєР°Р¶РґРѕРіРѕ Р¶Р°РЅСЂР° Рё СЃСЂРµРґРЅРёР№ Р±СЋРґР¶РµС‚ РїРѕ Р¶Р°РЅСЂСѓ. 
 select count(movies.title), genres.title, avg(movies.budget) from movies join genres on genres.id = genres_id group by genres.title;
--- 10. Найти и удалить комедию "Дикие истории" 2014-го года. 
-delete movies from movies join genres on genres.id = movies.genres_id where genres.title = 'комедия' and movies.title = 'Дикие истории' and movies.year = 2014;
+-- 10. РќР°Р№С‚Рё Рё СѓРґР°Р»РёС‚СЊ РєРѕРјРµРґРёСЋ "Р”РёРєРёРµ РёСЃС‚РѕСЂРёРё" 2014-РіРѕ РіРѕРґР°. 
+delete movies from movies join genres on genres.id = movies.genres_id where genres.title = 'РєРѕРјРµРґРёСЏ' and movies.title = 'Р”РёРєРёРµ РёСЃС‚РѕСЂРёРё' and movies.year = 2014;
